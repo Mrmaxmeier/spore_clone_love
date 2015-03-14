6510cee0e18198c7e9eb76178dffad64679351fe
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-03-14 19:27:13 +0100

    some more conf.lua

--- conf.lua
+++ conf.lua
@@ -5,4 +5,21 @@ function love.conf(t)
 	t.version = "0.9.2"
 	t.window.minwidth = 800
 	t.window.minheight = 600
+
+
+	t.modules.audio         = false
+	t.modules.event         = true
+	t.modules.graphics      = true
+	t.modules.image         = true
+	t.modules.joystick      = true
+	t.modules.keyboard      = true
+	t.modules.math          = true
+	t.modules.mouse         = true
+	t.modules.physics       = false
+	t.modules.sound         = true
+	t.modules.system        = true
+	t.modules.timer         = true
+	t.modules.window        = true
+
+	io.stdout:setvbuf("no")
 end
