807ad7ac9645d770b5dd4f6533d7a183ba73cc9f
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-03-24 14:07:32 +0100

    lowercase names are more linux friendly

--- parts/loadAll.lua
+++ parts/loadAll.lua
@@ -2,7 +2,7 @@ print("loading all parts")
 --local path = love.filesystem.getWorkingDirectory() .. "/parts/"
 --local parts = love.filesystem.getDirectoryItems(path)
 -- TODO: load all parts dynamically
-local parts = {"Body", "SpeedyFin", "AgileFin", "Mouth", "Eye"}
+local parts = {"body", "speedyfin", "agilefin", "mouth", "eye"}
 for i, v in ipairs(parts) do
 	local part = require("parts."..v)
 	registerPart(part)
